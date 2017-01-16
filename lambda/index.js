38624e5a5837f3f3f517c98bd76838fd5d0c9709
Tyler Small <email": null>
2017-01-16 10:13:05 -0600

    Enable pub/sub services through pub/nub.

--- lambda/index.js
+++ lambda/index.js
@@ -1,23 +1,25 @@
 var pubnub = require("pubnub")({
     ssl           : true,  // <- enable TLS Tunneling over TCP
-    publish_key   : process.env.PUB_NUB_PUBLISH_KEY,
-    subscribe_key : process.env.PUB_NUB_SUBSCRIBE_KEY
+    // Un-comment for production
+    // publish_key   : process.env.PUB_NUB_PUBLISH_KEY,
+    publish_key   : "pub-c-5c099dc9-4fb8-41d4-a462-54224a4f47d7",
+    // subscribe_key : process.env.PUB_NUB_SUBSCRIBE_KEY
+    subscribe_key : "sub-c-78d941e8-d9b7-11e6-b9cf-02ee2ddab7fe"
 });
-
-// console.log(pubnub);
 
 // Sends out a PubNub notification to clients to control the drones with commands
 function sendTheDrones(callback){
-
     /* ---------------------------------------------------------------------------
     Publish Messages
     --------------------------------------------------------------------------- */
     var message = { "Hello" : "World!" };
     pubnub.publish({
-        channel   : channelId,
+        // Uncomment for production
+        // channel   : process.env.PUB_NUB_CHANNEL_KEY,
+        channel   : 'my_channel',
         message   : message,
         callback  : function(e) {
-            console.log( "SUCCESS!", e );
+            console.log( "SUCCESS!", e);
         },
         error     : function(e) {
             console.log( "FAILED! RETRY PUBLISH!", e );
