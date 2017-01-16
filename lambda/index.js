1568913c56a6fa32f43d5b1a875c4b6395c7ef75
Tyler Small <email": null>
2017-01-16 11:41:14 -0600

    Refactor intent handlers to use ENV variables. Add dotenv dependencies.

--- lambda/index.js
+++ lambda/index.js
@@ -1,54 +1,11 @@
+var helpers = require('./response_helpers');
+var intentHandlers = require('./intent_handlers');
+
 var pubnub = require("pubnub")({
     ssl           : true,  // <- enable TLS Tunneling over TCP
-    // Un-comment for production
-    // publish_key   : process.env.PUB_NUB_PUBLISH_KEY,
-    publish_key   : "pub-c-5c099dc9-4fb8-41d4-a462-54224a4f47d7",
-    // subscribe_key : process.env.PUB_NUB_SUBSCRIBE_KEY
-    subscribe_key : "sub-c-78d941e8-d9b7-11e6-b9cf-02ee2ddab7fe"
+    publish_key   : process.env.PUB_NUB_PUBLISH_KEY,
+    subscribe_key : process.env.PUB_NUB_SUBSCRIBE_KEY
 });
-var helpers = require('./response_helpers');
-
-// Handlers for the commands/behaviors
-function sendTheDrones(callback){
-    /* ---------------------------------------------------------------------------
-    Publish Messages
-    --------------------------------------------------------------------------- */
-    var message = { "Command" : "fly" };
-    pubnub.publish({
-        // Uncomment for production
-        // channel   : process.env.PUB_NUB_CHANNEL_KEY,
-        channel   : 'drone_control',
-        message   : message,
-        callback  : function(e) {
-            console.log( "SUCCESS!", e);
-        },
-        error     : function(e) {
-            console.log( "FAILED! RETRY PUBLISH!", e );
-        }
-    });
-
-    const sessionAttributes = {};
-
-    callback(sessionAttributes,
-        helpers.buildSpeechletResponse('Drones', 'okay, I\'m sending the drones', null, false));
-}
-
-/**
- * Called when the user specifies an intent for this skill.
- */
-function onIntent(intentRequest, session, callback) {
-    console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
-
-    const intent = intentRequest.intent;
-    const intentName = intentRequest.intent.name;
-
-    // Dispatch to your skill's intent handlers
-    if (intentName === 'Start'){
-        sendTheDrones(callback);
-    }else {
-        throw new Error('Invalid intent');
-    }
-}
 
 // --------------- Main handler -----------------------
 
@@ -79,7 +36,7 @@ exports.handler = (event, context, callback) => {
                     callback(null, helpers.buildResponse(sessionAttributes, speechletResponse));
                 });
         } else if (event.request.type === 'IntentRequest') {
-            onIntent(event.request,
+            intent_handlers.onIntent(event.request,
                 event.session,
                 (sessionAttributes, speechletResponse) => {
                     callback(null, helpers.buildResponse(sessionAttributes, speechletResponse));
