3cef458db0e217fad338578fc0cd806dafb13587
Tyler Small <email": null>
2017-01-17 14:14:08 -0600

    Remove redundent files in test.

--- src/test.js
+++ src/test.js
@@ -1,57 +1,10 @@
 require('dotenv').load();
-var responseHelpers = require('./response_helpers');
-var intentHandlers = require('./intent_handlers');
-var sessionHelpers = require('./session_helpers');
-
-// --------------- Main handler -----------------------
-
-// Route the incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
-function testCall(event, context, callback){
-  try {
-      console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
-
-      /**
-       * Uncomment this if statement and populate with your skill's application ID to
-       * prevent someone else from configuring a skill that sends requests to this function.
-       */
-
-      if (event.session.application.applicationId !== process.env.AMAZON_APP_KEY) {
-          callback('Invalid Application ID');
-      }
-
-
-      if (event.session.new) {
-          sessionHelpers.onSessionStarted({ requestId: event.request.requestId }, event.session);
-      }
-
-      if (event.request.type === 'LaunchRequest') {
-          console.log('Intent Requested!');
-          onLaunch(event.request,
-              event.session,
-              (sessionAttributes, speechletResponse) => {
-                  callback(null, responseHelpers.buildResponse(sessionAttributes, speechletResponse));
-              });
-      } else if (event.request.type === 'IntentRequest') {
-          intentHandlers.onIntent(event.request,
-              event.session,
-              (sessionAttributes, speechletResponse) => {
-                  callback(null, responseHelpers.buildResponse(sessionAttributes, speechletResponse));
-              });
-      } else if (event.request.type === 'SessionEndedRequest') {
-          onSessionEnded(event.request, event.session);
-          callback(null, 'Thank you for flying with SkyNet');
-      }
-  } catch (err) {
-      callback(err);
-  }
-}
 
 var event = {
   "session": {
     "sessionId": "SessionId.6fa59c36-5660-4cba-83f1-89d33abbcd50",
     "application": {
-      "applicationId": "amzn1.ask.skill.c7a33919-96a2-496e-8730-5a795285f0d6"
+      "applicationId": process.env.AMAZON_APP_KEY
     },
     "attributes": {},
     "user": {
@@ -74,6 +27,8 @@ var event = {
 
 var context = {}
 
+// Callback function upon request (Customize as needed for debugging application)
 var hollaBaq = function(err){ console.log(err); };
 
-testCall(event, context, hollaBaq);
+// Calls the main application handler
+require('./index').handler(event, context, hollaBaq);
