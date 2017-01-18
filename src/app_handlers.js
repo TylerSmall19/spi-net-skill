a7e5cab8aad71d1edb72451a18c34d1ad2f172b5
Tyler Small <email": null>
2017-01-18 16:50:47 -0600

    Add session start handling

--- src/app_handlers.js
+++ src/app_handlers.js
@@ -1,9 +1,15 @@
 'use strict';
 var sessionHelpers = require('./session_helpers');
+var intentHandlers = require('./intent_handlers');
 
 module.exports = {
   onLaunch: function (launchRequest, session, callback) {
     // console.log(`onLaunch requestId=${launchRequest.requestId}, sessionId=${session.sessionId}`);
+    var command = {
+      command: '',
+      message: 'Welcome to the Skynet Drone interface. The drones are at your command.',
 
+    }
+    intentHandlers.sendCommand(callback, command);
   }
 }
